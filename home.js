db9d293dc8948c74eb0ade973e1f68d976609806
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2024-02-25 01:51:29 +0100

    Added new Renders

--- home.js
+++ home.js
@@ -1,21 +1,21 @@
 export const home = () => {
-	// let asd = "asd_asd_300x300.png"
-	// console.log(asd.substring(0, asd.length-12) + asd.substring(asd.length-4, asd.length));
+	let asd = "ArisoftGun_8_405x228.png"
+	console.log(asd.substring(0, asd.length-12) + asd.substring(asd.length-4, asd.length));
 
 	document.getElementById("3d").addEventListener("click", (e) => {
 		document.getElementById("3d-start").scrollIntoView({ behavior: "smooth" })
 	})
-    document.querySelector(".image-container").addEventListener("click", (e) => {
-		if (e.target.nodeName == "IMG") {
-			document.getElementById("selected-image").src = e.target.src.substring(0, e.target.src.length-12) + e.target.src.substring(e.target.src.length-4, e.target.src.length)
-			document.getElementById("selected-image").focus()
-			document.getElementById("overlay").classList.remove("opacity-0")
-			document.getElementById("overlay").classList.add("opacity-100")
-			document.getElementById("overlay").classList.remove("pointer-events-none")
-			document.querySelector(".pointer-events-auto").classList.add("cursor-pointer")
-			
-
-		}
+    document.querySelectorAll(".image-container").forEach((element) => {
+		element.addEventListener("click", (e) => {
+			if (e.target.nodeName == "IMG") {
+				document.getElementById("selected-image").src = e.target.src.substring(0, e.target.src.length-12) + e.target.src.substring(e.target.src.length-4, e.target.src.length)
+				document.getElementById("selected-image").focus()
+				document.getElementById("overlay").classList.remove("opacity-0")
+				document.getElementById("overlay").classList.add("opacity-100")
+				document.getElementById("overlay").classList.remove("pointer-events-none")
+				document.querySelector(".pointer-events-auto").classList.add("cursor-pointer")
+			}
+		})
 	})
 		
 
